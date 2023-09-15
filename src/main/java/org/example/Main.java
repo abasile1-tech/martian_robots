db33edca0c60f7fd2d02118ffd2e6c0d53707607
John <ashton.basile@gmail.com>
2023-09-15 14:50:42 +0100

    I made a Robot class and added C3PO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package main.java.org.example;
 public class Main {
   public static void main(String[] args) {
     System.out.print("Hello and welcome!\n");
+    Robot c3po = new Robot("C3PO");
+    c3po.printName();
   }
 }
---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,13 @@
+package main.java.org.example;
+
+public class Robot {
+  private String name;
+
+  public Robot(String name) {
+    this.name = name;
+  }
+
+  public void printName() {
+    System.out.println(this.name);
+  }
+}
