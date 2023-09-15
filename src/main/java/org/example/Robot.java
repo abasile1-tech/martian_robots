8e661c45be6428d77d6123e655a18301ae57593d
John <ashton.basile@gmail.com>
2023-09-15 14:57:39 +0100

    I added some getters and setters to the Robot class

--- src/main/java/org/example/Robot.java
+++ src/main/java/org/example/Robot.java
@@ -2,6 +2,9 @@ package main.java.org.example;
 
 public class Robot {
   private String name;
+  private int xPosition = 0;
+  private int yPosition = 0;
+  private char orientation = 'N';
 
   public Robot(String name) {
     this.name = name;
@@ -9,5 +12,29 @@ public class Robot {
 
   public void printName() {
     System.out.println(this.name);
+  }
+
+  public int getXPosition() {
+    return this.xPosition;
+  }
+
+  public int getYPosition() {
+    return this.yPosition;
+  }
+
+  public int getOrientation() {
+    return this.orientation;
+  }
+
+  public void setXPosition(int xPosition) {
+    this.xPosition = xPosition;
+  }
+
+  public void setYPosition(int yPosition) {
+    this.yPosition = yPosition;
+  }
+
+  public void setOrientation(char orientation) {
+    this.orientation = orientation;
   }
 }
